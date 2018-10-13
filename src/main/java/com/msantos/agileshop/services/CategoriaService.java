beda1803205d65d430149dac386052b5697ee59e
marcio santos <marciosantosf7@gmail.com>
2018-10-13 01:02:16 -0300

    Deletando Categorias com o DELETE

--- src/main/java/com/msantos/agileshop/services/CategoriaService.java
+++ src/main/java/com/msantos/agileshop/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.msantos.agileshop.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.msantos.agileshop.domain.Categoria;
+import com.msantos.agileshop.exceptions.DataIntegrityException;
 import com.msantos.agileshop.exceptions.ObjectNotFoundException;
 import com.msantos.agileshop.repositories.CategoriaRepository;
 
@@ -30,6 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+		
 	}
 	
 }
